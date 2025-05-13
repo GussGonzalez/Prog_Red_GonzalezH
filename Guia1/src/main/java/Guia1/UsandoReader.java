80826f6ad17683918b4dfbc135d06a889097bb40
GussGonzalez <gus.g.backup@gmail.com>
2025-05-12 21:39:57 -0300

    Problema de ejercicio2 solucionado
    Actualización del ejercicio 2a con la solución a los problemas de input,. Incompleto.

--- Guia1/src/main/java/Guia1/UsandoReader.java
+++ Guia1/src/main/java/Guia1/UsandoReader.java
@@ -11,37 +11,49 @@ public class UsandoReader {
 			PrintStream ps = new PrintStream(System.out);
 			PrintStream  psErr = new PrintStream(System.err);
 			BufferedReader br = new BufferedReader (isr);
-			
 	
 	public void ejercicio2A() {
-		String apellidos [] = null;
-		char ch = 0;
-		int secondI = -1;
-		
 		try {
-			ps.println("Ingrese tres apellidos separados por un espacio: ");
-			String apellido = br.readLine();
-			
-			if (apellido != null && !apellido.isEmpty()) {
-            } else {
-                psErr.println("Por favor, ingrese una opción válida.");
-            }
-			
-			/*
-			for (int i = 0 ; i < apellido.length(); i++) {
-				ch = apellido.charAt(i);
-				if (ch != 32) {
-					apellidos[secondI] = apellidos[secondI] + String.valueOf(ch);
-				}else {
-					secondI +=1;
-				}
-				ps.println(apellidos);
+			if (System.in.available() > 0) {
+			    br.readLine(); // flush leftover line
 			}
-			*/
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}//end try/catch
+
+		
+		String[] apellidos = new String[3];
+		char ch = 0;
+		int index = 0;
+		String apellido = "";
+		
+		try {
+			ps.println("Ingrese tres apellidos separados por un espacio: ");
+			apellido = br.readLine();
+			ps.println(apellido);
+			
+			if (apellido != null && !apellido.isEmpty()) {
+				
+				for (int i = 0 ; i < apellido.length(); i++) {
+					ch = apellido.charAt(i);
+					if (ch != 32) {
+						apellidos[index] = apellidos[index] + String.valueOf(ch);
+					}else if (index > 2){
+						index +=1;
+					}//end if/else
+					ps.println(apellidos);
+				}//end for
+				
+            } else {
+                psErr.println("Por favor, ingrese un valor válido.");
+            }//end if/else de verificación
+
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//end try/catch
 	}//end ejercicio2A
 
 }//end UsandoReader
