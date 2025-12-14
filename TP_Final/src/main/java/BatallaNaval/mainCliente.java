5f5cc866ebd1f18aa13d7301505a3136ee31b3bd
GussGonzalez <gus.g.backup@gmail.com>
2025-12-14 13:17:30 -0300

    TP_Final - Empezando de 0, individual

--- TP_Final/src/main/java/BatallaNaval/mainCliente.java
+++
@@ -1,9 +0,0 @@
-package BatallaNaval;
-
-public class mainCliente {
-	public static void main(String[] args) {
-		new Cliente().iniciar();
-	}//end main
-	
-
-}//mainCliente
--- TP_Final/src/main/java/Modelo/TipoCelda.java
+++
@@ -1,8 +0,0 @@
-package Modelo;
-
-public enum TipoCelda {
-    AGUA,    
-    BARCO,   
-    IMPACTO, 
-    FALLO    
-}//TipoCelda
